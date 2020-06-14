8f7c00eec94090eb54569d09939cc1744fb0c85a
Zack Rose <zackrose21@gmail.com>
2020-06-14 12:57:44 +0100

    MVP

--- src/Box.js
+++ src/Box.js
@@ -3,18 +3,15 @@ import './Box.css'
 import {choice} from './helpers';
 
 class Box extends Component {
-    static defaultProps = {
-        allColors: ['purple','magenta','violet','pink']
-    }
     constructor(props) {
         super(props);
-        this.state = {color: choice(this.props.allColors)};
+        this.state = {color: choice(this.props.colors)};
         this.handleClick = this.handleClick.bind(this);
     }
     pickColor(){
         let newColor;
         do {
-            newColor = choice(this.props.allColors)
+            newColor = choice(this.props.colors)
         } while (newColor === this.state.color)
         this.setState({color: newColor})
     }
--- src/BoxContainer.js
+++ src/BoxContainer.js
@@ -4,11 +4,12 @@ import './BoxContainer.css';
 
 class BoxContainer extends Component {
     static defaultProps = {
-        numBoxes: 18
+        numBoxes: 18,
+        allColors: ['purple','magenta','violet','pink']
     }
     render(){
         const boxes = Array.from({length: this.props.numBoxes}).map(() =>(
-        <Box/>
+        <Box colors={this.props.allColors}/>
         ));
         return <div className = "BoxContainer">{boxes}</div>
     }
